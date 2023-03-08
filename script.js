54cfd8a82016583b7d58256a7e9286a89aa2114d
æ <81041745+piggydoggie@users.noreply.github.com>
2023-03-08 17:22:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Passaporto Tweaker
+// @version      0.1
+// @description  a script to understand better the availability when requesting a new passport in Italy
+// @author       giorgiodalessandro@aol.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if (!window.location.href.startsWith("https://www.passaportonline.poliziadistato.it/CittadinoAction.do?codop=resultRicercaRegistiProvincia")) return undefined;
+    if (!document?.getElementById('infos')?.children[2].children?.length) return undefined;
+
+    let table = document?.getElementById('infos')?.children[2].children
+
+    const toSkip = [
+        "COMMISSARIATO MONTE MARIO",
+        "COMMISSARIATO PRENESTINO",
+        "COMMISSARIATO LIDO DI ROMA"
+    ];
+
+    for (let i = 0; i < table?.length; i++) {
+        let iteration = table?.item(i);
+
+        const rowCity = iteration?.children?.item(0)?.innerText
+        const rowProvince = iteration?.children?.item(3);
+        const rowAvailability = iteration?.children?.item(5);
+        const rowHref = iteration?.children.item(6)?.children[0]?.getAttribute('href')?.split("data=");
+
+        if (
+            rowProvince?.innerHTML != "ROMA" ||
+            rowAvailability?.innerHTML != "Si" ||
+            !rowHref ||
+            !rowAvailability ||
+            !!toSkip.find(e => e == rowCity)
+        ) {
+            continue;
+        }
+
+        rowAvailability.innerHTML = rowHref[1];
+    }
+    console.log("website tweaked")
+})();
